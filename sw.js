d15e3e4e9f1bbe26b1044b22c1e8a33f97290545
jasperlow <jasperlow@nets.com.sg>
2026-03-01 18:54:41 +0800

    upgrade sw for pwa

--- sw.js
+++ sw.js
@@ -1,4 +1,29 @@
-const CACHE='photo-stitcher-v1';
-const FILES=['index.html','photo-stitcher.html','manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='photo-stitcher-v1.0.2';
+const FILES=['index.html','photo-stitcher.html','save.html','manifest.json','icon-192.png','icon-512.png'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    )).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(cached=>{
+      if(cached)return cached;
+      return fetch(e.request).then(response=>{
+        if(response&&response.status===200){
+          const clone=response.clone();
+          caches.open(CACHE).then(c=>c.put(e.request,clone));
+        }
+        return response;
+      });
+    })
+  );
+});
