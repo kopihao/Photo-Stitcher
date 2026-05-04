c9cb5ff91d715742d64465a79eb3db33236c46c2
jasperlow <jasperlow@nets.com.sg>
2026-05-04 23:51:26 +0800

    Enforce PWA behavior

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE='photo-stitcher-v1.0.2';
+const CACHE_NAME='photo-stitcher-v1.0.2';
 const FILES=['index.html','photo-stitcher.html','save.html','manifest.json','icon-192.png','icon-512.png'];
 
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
 });
 
 self.addEventListener('activate',e=>{
   e.waitUntil(
     caches.keys().then(keys=>Promise.all(
-      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
     )).then(()=>self.clients.claim())
   );
 });
@@ -20,7 +20,7 @@ self.addEventListener('fetch',e=>{
       return fetch(e.request).then(response=>{
         if(response&&response.status===200){
           const clone=response.clone();
-          caches.open(CACHE).then(c=>c.put(e.request,clone));
+          caches.open(CACHE_NAME).then(c=>c.put(e.request,clone));
         }
         return response;
       });
